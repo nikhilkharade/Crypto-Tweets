ae06965dbcc3f0d5001be64e6292b04adab18f3a
Nikhil Kharade <nikhil@gokwik.co>
2021-12-27 15:46:55 +0530

    removed commented code

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# @app.get("/ping")
-# async def ping():
-#     return 'Hello World!'
-
 @app.get("/ping")
 async def ping():
     return {'msg':'pong'}
